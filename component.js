12d177362c63732086c1dd8bd1921e69eb46d097
Albert Lorenzo Segarra <prueba.usabilidad.2023@gmail.com>
2023-01-19 12:20:59 +0100

    first commit
    first commit

---
+++ component.js
@@ -0,0 +1,62 @@
+class Component {
+    constructor(title, q1, q2, q3, q4, id) {
+        this.check = false;
+        this.title = title;
+        this.a1 = q1;
+        this.a2 = q2;
+        this.a3 = q3;
+        this.a4 = q4;
+        this.id = id + 1;
+    }
+
+    #randomize(value) {
+        const questions = [this.a1, this.a2, this.a3, this.a4];
+        if (value) questions.shuffle(); 
+        return `
+            <li data-value='${questions[0].correcta}'>${questions[0].enunciado}</li>
+            <li data-value='${questions[1].correcta}'>${questions[1].enunciado}</li>
+            <li data-value='${questions[2].correcta}'>${questions[2].enunciado}</li>
+            <li data-value='${questions[3].correcta}'>${questions[3].enunciado}</li>
+        `;
+    }
+
+    getComponent(randomizeValue) {
+        return `<div class='card' id='${this.id}'>
+                              <h1>${this.title}</h1>
+                                  <ul id='ul-${this.id}'>
+                                    ${this.#randomize(randomizeValue)}
+                                  </ul>
+                              </div>`;
+    }
+
+    #checkAnswere(li) {
+        if (li.getAttribute("data-value") === "true") {
+            li.style.textDecoration = "underline";
+            li.style.backgroundColor = "#3dfc89";
+            if (!this.check) {
+                document.getElementById("c-score").innerHTML =
+                    parseInt(document.getElementById("c-score").innerHTML) + 1;
+            }
+        }
+
+        if (li.getAttribute("data-value") === "false") {
+            li.style.textDecoration = "line-through";
+            if (!this.check) {
+                document.getElementById("i-score").innerHTML =
+                    parseInt(document.getElementById("i-score").innerHTML) + 1;
+            }
+        }
+
+        this.check = true;
+    }
+
+    setCorrectTrigger() {
+      const liArray = Array.from(
+        document.getElementById(`ul-${this.id}`).children
+      );
+
+      liArray.forEach((e) => {
+        e.addEventListener("click", this.#checkAnswere.bind(this, e));
+      });
+    }
+}
---
+++ main.js
@@ -0,0 +1,99 @@
+Object.defineProperty(Document.prototype, "appendComponentToDOM", {
+    value: function (component) {
+        const wrapper = this.getElementById("card-wrapper");
+        wrapper.insertAdjacentHTML("beforeend", component);
+    },
+});
+
+Object.defineProperty(Document.prototype, "play", {
+    value: function (data, checkBoxValue, iniRange, endRange) {
+        let cardStack = Object.entries(data).slice(iniRange, endRange);
+        if (checkBoxValue) cardStack.shuffle();
+        let id = 0;
+        for (const card of cardStack) {
+            let component = new Component(card[1].pregunta, card[1].q1, card[1].q2, card[1].q3, card[1].q4, id);
+            this.appendComponentToDOM(component.getComponent());
+            component.setCorrectTrigger();
+            id++;
+        }
+    },
+});
+
+Object.defineProperty(Document.prototype, "prepareData", {
+    value: function () {
+        const raw_data = this.getElementById('raw-data').innerText.replace(/"/g, '');
+
+        // Replace all double quotes with single quotes
+        const splitted_data = raw_data.split(';').map((e) => e.split(','));
+
+        // Replace spaces in the beginning and end of each element
+        for (let i = 0; i < splitted_data.length; i++) {
+            for (let j = 0; j < splitted_data[i].length; j++) {
+                splitted_data[i][j] = splitted_data[i][j].trim();
+            }
+        }
+
+        // delete the first element in splitted_data array
+        splitted_data.shift();
+
+        // delete the last element in splitted_data array
+        splitted_data.pop();
+
+        // Construct the global main data object
+        const data = {};
+        for (let i = 0; i < splitted_data.length; i++) {
+            data[i] = {
+                'tema': splitted_data[i][0],
+                'pregunta': splitted_data[i][1],
+            }
+
+            for (let j = 2; j < splitted_data[i].length - 1; j++) {
+                data[i][`q${j - 1}`] = {
+                    'enunciado': splitted_data[i][j],
+                    'correcta': splitted_data[i][splitted_data[i].length - 1] == splitted_data[i][j] ? true : false
+                }
+            }
+        }
+
+        return data;
+    },
+});
+
+
+Object.defineProperty(Array.prototype, "shuffle", {
+    value: function () {
+        for (let i = this.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this[i], this[j]] = [this[j], this[i]];
+        }
+    },
+});
+
+const setChanges = function (data) {
+    let checkBoxValue = this.getElementById('randomize-checkbox').checked;
+    let iniRange = parseInt(this.getElementById('ini-value').value);
+    let endRange = parseInt(this.getElementById('end-value').value);
+
+    if (iniRange < 0 || endRange > 71 || iniRange > endRange) {
+        alert('Rango [0, 48]');
+        return;
+    } else if (iniRange == endRange) {
+        alert('Rango [0, 48]');
+        return;
+    }
+    
+    this.getElementById('card-wrapper').innerHTML = '';
+    this.play(data, checkBoxValue, iniRange, endRange);
+    this.getElementById('c-score').innerHTML = 0;
+    this.getElementById('i-score').innerHTML = 0;
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    let data = this.prepareData();
+
+    // convert data to an array
+    data = Object.values(data);
+
+    this.play(data, 0, 0, 71);
+    this.getElementById('btn-set-changes').addEventListener('click', setChanges.bind(this, data));
+});
